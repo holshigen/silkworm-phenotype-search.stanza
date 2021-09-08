9c280a69df259bd5d4b2a5333ce496c3ab16bf00
Masakazu Saga <saga@localhost.localdomain>
2021-09-08 15:52:29 +0900

    Add silkworm phenotype stanza

---
+++ stanzas/silkworm-phenotype/README.md
@@ -0,0 +1,3 @@
+# Silkworm Phenotype
+
+Stanza description goes here. Edit `stanzas/silkworm-phenotype/README.md` to update.
---
+++ stanzas/silkworm-phenotype/index.js
@@ -0,0 +1,14 @@
+import Stanza from 'togostanza/stanza';
+
+export default class SilkwormPhenotype extends Stanza {
+  async render() {
+    this.renderTemplate(
+      {
+        template: 'stanza.html.hbs',
+        parameters: {
+          greeting: `Hello, ${this.params['say-to']}!`
+        }
+      }
+    );
+  }
+}

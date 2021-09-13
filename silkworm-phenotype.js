d6a1d80b9aabd62c66eb023e50c9100b394f7041
masakazu93 <masakazu93@users.noreply.github.com>
2021-09-13 08:31:03 +0000

    deploy: 712f3bf5b34f4f83fa1fcc970f8afb01ee6876db

--- silkworm-phenotype.js
+++ silkworm-phenotype.js
@@ -1898,14 +1898,14 @@ class Hello extends Stanza {
             });
 
             // URI表示チェックボックス
-            this.root.querySelector("#checkbox").change(function() {
-                var checked = $(this).prop('checked');
-                if (checked) {
-                    this.root.querySelectAll("#uri").show();
-                } else {
-                    this.root.querySelectAll("#uri").hide();
-                }
-            });
+            //this.root.querySelector("#checkbox").change(function() {
+            //    var checked = $(this).prop('checked');
+            //    if (checked) {
+            //        this.root.querySelectAll("#uri").show();
+            //    } else {
+            //        this.root.querySelectAll("#uri").hide();
+            //    }
+            //});
 
         } catch (e) {
             console.log(e);

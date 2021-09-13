b766da0264bd0dc22fbcb7f9c28d1ec28cd54261
masakazu93 <33208651+masakazu93@users.noreply.github.com>
2021-09-13 16:06:03 +0900

    Update index.js

--- stanzas/silkworm-phenotype/index.js
+++ stanzas/silkworm-phenotype/index.js
@@ -40,6 +40,12 @@ export default class Hello extends Stanza {
             //        this.root.querySelectAll("#uri").hide();
             //    }
             //});
+const checkboxElement = document.getElementsById('checkbox');
+
+checkboxElement.addEventListener('valueChanged', (event) => {
+  console.log('event received', event);
+  console.log(event.detail); // {value: 42}
+});
 
         } catch (e) {
             console.log(e);

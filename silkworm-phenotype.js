8236184e5fa222ec126199bc8c3ac3a9b59acc83
masakazu93 <masakazu93@users.noreply.github.com>
2021-09-15 05:19:54 +0000

    deploy: 73a663edff8b8c1d2e72e38226d8a54c9d2925dd

--- silkworm-phenotype.js
+++ silkworm-phenotype.js
@@ -1888,12 +1888,13 @@ class Hello extends Stanza {
                 template : rq,
                 parameters : this.params,
             });
+            var data2 = unwrapValueFromBinding(result2);
 
             this.renderTemplate({
                 template: 'stanza.html.hbs',
                 parameters: {
                     silkworm_phenotype_uri: unwrapValueFromBinding(result1),
-                    silkworm_phenotype_search: unwrapValueFromBinding(result2)
+                    silkworm_phenotype_search: data2
                 }
             });
 

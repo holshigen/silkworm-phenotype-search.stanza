5e7755001bb00fa67f5007c7cf8648bb9435ee7a
holshigen <90878126+holshigen@users.noreply.github.com>
2021-09-21 11:45:41 +0900

    Update index.js

--- stanzas/silkworm-phenotype-search/index.js
+++ stanzas/silkworm-phenotype-search/index.js
@@ -52,7 +52,7 @@ export default class SilkwormPhenotypeSearch extends Stanza {
             $(this.root.querySelector('#resultTable')).dataTable({
             //    "aLengthMenu" : [ 10, 25, 50, 100 ], // 表示件数の選択肢
             //    "iDisplayLength" : 10, // 表示件数のデフォルトの値
-                "ordering" : treu, // ソート
+                "ordering" : true, // ソート
                 "searching" : false, // 検索
                 "oLanguage" : { // 表示される文字
                     "sEmptyTable" : "No data found.",
